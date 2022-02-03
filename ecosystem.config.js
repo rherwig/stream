24b3786fa30a5d4a6999a0fc9b3215be515f5ef1
Rico Herwig <rherwig4711@gmail.com>
2022-02-03 18:15:46 +0100

    Add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,52 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const {
+    SSH_HOST,
+    SSH_USER,
+    GIT_REPOSITORY,
+    GIT_BRANCH,
+    WEB_ROOT,
+} = process.env;
+
+module.exports = {
+    apps: [
+        {
+            name: 'stream-app',
+            script: 'server.js',
+            watch: [
+                'server.js',
+            ],
+            ignore_watch: [
+                'node_modules',
+                'var/**/*',
+                'dist',
+            ],
+            watch_options: {
+                followSymlinks: false,
+                usePolling: true,
+            },
+            env: {
+                NODE_ENV: 'development',
+            },
+            env_production: {
+                NODE_ENV: 'production',
+            },
+        },
+    ],
+
+    deploy: {
+        production: {
+            user: SSH_USER,
+            host: SSH_HOST,
+            ref: GIT_BRANCH,
+            repo: GIT_REPOSITORY,
+            path: WEB_ROOT,
+            ssh_options: 'ForwardAgent=yes',
+            'pre-deploy-local': '',
+            'post-deploy': 'npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+            'pre-setup': '',
+        },
+    },
+};
